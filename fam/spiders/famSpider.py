6a6362f44c26ebb557b5f522409686db3684afef
abdullatef trefi <abdullatef.trefi98@gmail.com>
2023-01-02 14:30:00 +0400

    add scraping blog

--- fam/spiders/famSpider.py
+++ fam/spiders/famSpider.py
@@ -4,7 +4,14 @@ from scrapy.linkextractors import LinkExtractor
 class FamSpider(CrawlSpider):
     name = "famspider"
     allowed_domains = ['famproperties.com']
-    start_urls = ["https://famproperties.com"]
+    start_urls = ["https://famproperties.com/blog/"]
     rules = (
-        Rule(LinkExtractor(allow="/")),
+        Rule(LinkExtractor(allow="blog",),callback = "parse_blog"),
+        # Rule()
     )
+
+    def parse_blog(self, response):
+        yield {
+            "title" : response.css('#project h1::text').get(),
+            "description" : response.css('#R12848563557498805588_report li .t-Comments-body .t-Comments-comment').get()
+        }
