d11433fbc05ecdba6eed4180f39beb0fadb0831e
abdullatef trefi <abdullatef.trefi98@gmail.com>
2023-01-02 15:36:41 +0400

    adding property buy

--- fam/spiders/famSpider.py
+++ fam/spiders/famSpider.py
@@ -4,9 +4,10 @@ from scrapy.linkextractors import LinkExtractor
 class FamSpider(CrawlSpider):
     name = "famspider"
     allowed_domains = ['famproperties.com']
-    start_urls = ["https://famproperties.com/blog/"]
+    start_urls = ["https://famproperties.com/"]
     rules = (
         Rule(LinkExtractor(allow="blog",),callback = "parse_blog"),
+        Rule(LinkExtractor(allow="buy",deny="properties-for-sale.html"),callback = "parse_buy"),
         Rule(LinkExtractor(), follow=True),
 
         # Rule()
@@ -15,5 +16,11 @@ class FamSpider(CrawlSpider):
     def parse_blog(self, response):
         yield {
             "title" : response.css('#project h1::text').get(),
-            "description" : response.css('#R12848563557498805588_report li .t-Comments-body .t-Comments-comment').get()
+            "description" : response.css('#R12848563557498805588_report li .t-Comments-body .t-Comments-comment::text').get()
+        }
+
+    def parse_buy(self,response):
+        yield{
+            "title" : response.css(".property-page__title::text").get(),
+            "description" : response.css(".property-description__text-trim::text").get()
         }
